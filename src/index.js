6667ad9927fef1c0ba7ccc524df5c8da3a089b8e
curiouscauliflower <131444476+curiouscauliflower@users.noreply.github.com>
2023-06-05 17:27:46 +0300

    Added some comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+//Search form
+function searchCity(city) {
+  let units = "metric";
+  let apiKey = "671758b590o71f73f4ceca7at502e7ba";
+  let apiEndpoint = "https://api.shecodes.io/weather/v1/current";
+  let apiUrl = `${apiEndpoint}?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showCurrentWeather);
+}
+
+function searchCityOfMaxMinTemp(city) {
+  let units = "metric";
+  let apiKey = "ad43b41d614c72843d4867ddcfa4c147";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showMaxMinTempInCurrentWeather);
+}
+
+function inputCity(event) {
+  event.preventDefault();
+  let searchField = document.querySelector("#search-field");
+  if (!searchField.value.trim()) return;
+
+  let city = searchField.value.trim();
+
+  searchCity(city);
+  searchCityOfMaxMinTemp(city);
+
+  searchField.value = "";
+}
+
+searchCity("Kherson");
+searchCityOfMaxMinTemp("Kherson");
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", inputCity);
+
 //Current Date
 function formatDate() {
   let current = new Date();
@@ -51,44 +87,7 @@ function formatTime(timestamp) {
   return `${currentHours}:${currentMinutes}`;
 }
 
-//Search form
-function searchCity(city) {
-  let units = "metric";
-  let apiKey = "671758b590o71f73f4ceca7at502e7ba";
-  let apiEndpoint = "https://api.shecodes.io/weather/v1/current";
-  let apiUrl = `${apiEndpoint}?query=${city}&key=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showCurrentWeather);
-}
-
-function searchCityOfMaxMinTemp(city) {
-  let units = "metric";
-  let apiKey = "ad43b41d614c72843d4867ddcfa4c147";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showMaxMinTempInCurrentWeather);
-}
-
-function inputCity(event) {
-  event.preventDefault();
-  let searchField = document.querySelector("#search-field");
-  if (!searchField.value.trim()) return;
-
-  let city = searchField.value.trim();
-
-  searchCity(city);
-  searchCityOfMaxMinTemp(city);
-
-  searchField.value = "";
-}
-
-searchCity("Kherson");
-searchCityOfMaxMinTemp("Kherson");
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", inputCity);
-
-//Geolocation
-
+//Description with a capital letter
 function showCurrentDescription(response) {
   let currentDescription = response.data.condition.description;
   let summary = document.querySelector("#summary");
@@ -96,6 +95,7 @@ function showCurrentDescription(response) {
     currentDescription.charAt(0).toUpperCase() + currentDescription.slice(1);
 }
 
+//Current Weather
 function showCurrentWeather(response) {
   document.querySelector("#date").innerHTML = formatDate();
 
@@ -118,12 +118,16 @@ function showCurrentWeather(response) {
   document.querySelector("#pressure").innerHTML = Math.round(response.data.temperature.pressure);
 }
 
+
+//Current Weather for High and Low Temp. from another API
 function showMaxMinTempInCurrentWeather(response) {
   document.querySelector("#max").innerHTML = Math.round(response.data.main.temp_max);
 
   document.querySelector("#min").innerHTML = Math.round(response.data.main.temp_min);
 }
 
+
+//Geolocation API
 function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
